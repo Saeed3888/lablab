3f9f78d146d09f4b2bfae4331af117b41177bf7f
Saeed5062 <Ssa5468@psu.edu>
2020-09-03 12:21:39 +0000

    11

---
+++ main.py
@@ -0,0 +1,20 @@
+#Author: Saeed Alyami, ssa5468@psu
+#Collaborator: Vincent Barnes, vjb5182@psu
+#collaaborator: Lucas Yancey, lcy5032@psu,edu
+tempo = input ("Enter temperature: ")
+corf = input ("Enter unit in F/f or C/c: ")
+tempo = float(tempo)
+if(corf =="F" or corf == "f"):
+  fahrenheit = tempo
+  celsius = (fahrenheit - 32) * (5/9)
+  celsius = str(celsius)
+  fahernheit = str(fahrenheit) 
+  print(f'{fahrenheit}° in Fahrenheit is equivalent to {celsius}° Celsius.') 
+elif(corf =="C" or corf == "c"): 
+  celsius = tempo
+  fahrenheit = celsius *1.8 + 32
+  celsius = str(celsius)
+  fahrenheit = str(fahrenheit)
+  print(f'{celsius}° in Celsius is equivalent to {fahrenheit}° Fahrenheit.')
+else:
+     print(f'Invalid unit({corf}).')
