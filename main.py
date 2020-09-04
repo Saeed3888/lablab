bb1c1b0f8d2093501d307a1a315fdb5bd8f02bf5
Saeed5062 <Ssa5468@psu.edu>
2020-09-04 22:25:07 +0000

    77

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-#Author: Saeed Alyami, ssa5468@psu
-#Collaborator: Vincent Barnes, vjb5182@psu
-#collaaborator: Lucas Yancey, lcy5032@psu,edu
+# Author: Saeed Alyami ssa5468@psu
+# Collaborator: Vincent Barnes vjb5182@psu
+# Collaborator: Lucas Yancey lcy5032@psu,edu
 tempo = input ("Enter temperature: ")
 corf = input ("Enter unit in F/f or C/c: ")
 tempo = float(tempo)
+
 if(corf =="F" or corf == "f"):
   fahrenheit = tempo
   celsius = (fahrenheit - 32) * (5/9)
   celsius = str(celsius)
   fahernheit = str(fahrenheit) 
   print(f'{fahrenheit}° in Fahrenheit is equivalent to {celsius}° Celsius.') 
+
 elif(corf =="C" or corf == "c"): 
   celsius = tempo
   fahrenheit = celsius *1.8 + 32
